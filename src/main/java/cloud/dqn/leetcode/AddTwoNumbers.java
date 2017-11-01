a7e7f74092c184dceaf899a9615b2db8f4fc1be0
Dan Quoc Nguyen <aviuswen@gmail.com>
2017-10-31 17:22:58 -0700

    Submitted java solution for AddTwoNumbers

--- src/main/java/cloud/dqn/leetcode/AddTwoNumbers.java
+++ src/main/java/cloud/dqn/leetcode/AddTwoNumbers.java
@@ -35,7 +35,86 @@ public class AddTwoNumbers {
 
     class Solution {
         public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            throw new NotImplementedException();
+        /*
+            use a carry;
+            for each list {
+                get the head value and progress it; if head is null; just use zero
+                add each head value and add the carry as well
+                modulo 10 to get the value to append to result
+                divide by 10 to write to carry
+            }
+
+        */
+            int carryOver = 0;
+
+            ListNode result = null;
+            ListNode tail = null;
+            // don't want to write over existing values
+            ListNode list1 = l1;
+            ListNode list2 = l2;
+            while (list1 != null || list2 != null) {
+                int val1 = 0;
+                int val2 = 0;
+                if (list1 != null) {
+                    val1 = list1.val;
+                    list1 = list1.next;
+                }
+                if (list2 != null) {
+                    val2 = list2.val;
+                    list2 = list2.next;
+                }
+                int combinedVal = val1 + val2 + carryOver;
+                carryOver = combinedVal / 10;
+                ListNode newNode = new ListNode(combinedVal % 10);
+                if (result == null) {
+                    result = newNode;
+                } else {
+                    tail.next = newNode;
+                }
+                tail = newNode;
+            }
+
+            // if carryOver != 0; then write the last node
+            // assumed that result should not be null as the givens stated that
+            // the two lists being added where non empty
+            if (carryOver != 0) {
+                ListNode lastNode = new ListNode(carryOver);
+                tail.next = lastNode;
+            }
+
+            return result;
+        }
+    }
+
+    // not needed but nice to have
+    public class Stack {
+        ListNode top;
+        public Stack() {
+            this.top = null; // class attributes default to null, but I want to be verbose
+        }
+        public Stack(ListNode listNode) {
+            this.top = null; // being verbose
+            while (listNode != null) {
+                push(listNode.val);
+            }
+        }
+        // null on empty rather than throwing an 'EmptyStackException'
+        public Integer pop() {
+            if (isEmpty()) {
+                return null;
+            } else {
+                int value = top.val;
+                top = top.next;
+                return value;
+            }
+        }
+        public void push(int val) {
+            ListNode listNode = new ListNode(val);
+            listNode.next = top;
+            top = listNode;
+        }
+        public boolean isEmpty() {
+            return top == null;
         }
     }
 }
