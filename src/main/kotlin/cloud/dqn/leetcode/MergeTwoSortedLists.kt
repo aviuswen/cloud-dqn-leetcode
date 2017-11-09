b98f93cddbcddf98f152b5c5ad6f4c4551034db1
Dan Quoc Nguyen <aviuswen@gmail.com>
2017-11-09 13:53:57 -0800

    MergeTwoSortedLists::Start

---
+++ src/main/kotlin/cloud/dqn/leetcode/MergeTwoSortedLists.kt
@@ -0,0 +1,24 @@
+package cloud.dqn.leetcode
+
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ * Merge two sorted linked lists and return it as a new list.
+ *  The new list should be made by splicing together the nodes
+ *  of the first two lists.
+ *
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int = 0) {
+ *     var next: ListNode? = null
+ * }
+ */
+class MergeTwoSortedLists {
+    class ListNode(var `val`: Int = 0) {
+        var next: ListNode? = null
+    }
+    class Solution {
+        fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+
+            return null
+        }
+    }
+}
